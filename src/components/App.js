d98dc69804220190798d05bd46a7df2c5695d94a
Jason Zhou <jzhou35@buffalo.edu>
2021-02-02 14:28:59 -0500

    Everything besides add sorting feature for comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import video from "../data/video.js";
+import VideoInfo from './VideoInfo';
 
 function App() {
   console.log("Here's your data:", video);
 
   return (
     <div className="App">
-      <iframe
+      <VideoInfo embedUrl={video.embedUrl} title={video.title} views={video.views} createdAt={video.createdAt} upvotes={video.upvotes} downvotes={video.downvotes} comments={video.comments}/>
+      {/* <iframe
         width="919"
         height="525"
         src="https://www.youtube.com/embed/dQw4w9WgXcQ"
         frameborder="0"
         allowfullscreen
         title="Thinking in React"
-      />
+      /> */}
     </div>
   );
 }
